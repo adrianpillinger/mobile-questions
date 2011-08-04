1b7f3ff48881e6b97720695e5b480934bc78c407
Adrian <adrian.pillinger@gmail.com>
2011-08-04 21:23:05 +0100

    Initial Project structure

---
+++ js/inspection.js
@@ -0,0 +1,48 @@
+var MobileQuestions = (function() {
+	
+	// main module
+ 	var MobileQuestions = {
+ 		Util : {
+ 			loadForm : function() {
+				$.ajax({
+					url: 'forms/form1.json',
+					success: function( data ) {
+						var questions = data['questions'];
+						var answerId;
+						
+						// form title
+						$('#questions').append('<h1>' + data['formName'] + '</h1>');
+						
+						// questions
+						for (i=0;i<questions.length;i++)
+						{
+							var thisQuestion = questions[i];
+							$('#questions').append('<p/>' + thisQuestion["question"]);
+							if (thisQuestion["answerType"] === "ENUM")
+							{
+								answerId = "answer_"+i;
+								$('#questions').append("<input id='"+answerId+"'></div>");
+								$('#'+answerId).autocomplete({
+									source: thisQuestion["values"]
+								});
+							}
+						}
+					},
+					error: function(data) {
+						alert('Error Status: ' + data["statusText"]);
+					}
+				});
+			}
+  		}
+ 	}
+
+	return MobileQuestions;
+
+})();
+
+var MobileQuestionsUtils = MobileQuestions.Util;
+
+$(document).ready(function() {
+	MobileQuestionsUtils.loadForm();
+});
+
