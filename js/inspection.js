2dfa231e7b794ff1510c521a8387bceffb2526a1
Adrian <adrian.pillinger@gmail.com>
2011-08-07 20:38:41 +0100

    Need to try in webserver

--- js/inspection.js
+++ js/inspection.js
@@ -1,54 +1,53 @@
 
 
 var MobileQuestions = (function() {
-//	var createQuestion = function(question){ return "TEST"; };
 	// main module
  	var MobileQuestions = {
- 		Util : {
- 			loadForm : function() {
-				$.ajax({
-					url: 'forms/form1.json',
-					success: function( data ) {
-						var questions = data['questions'];
-						var answerId;
-						var thisQuestion;
-                        
-						// form title
+ 	
+		loadForm : function() {
+			$.ajax({
+				url: 'forms/form1.json',
+				success: function( data ) {
+					var questions = data['questions'];
+					var answerId;
+					var thisQuestion;
+					
+					// form title
 //						$('#title').append('<h1>' + data['formName'] + '</h1>');
-						
-						// questions
-						for (i=0;i<questions.length;i++)
-						{
-							thisQuestion = questions[i];
+					
+					// questions
+					for (i=0;i<questions.length;i++)
+					{
+						thisQuestion = questions[i];
+						createQuestion(thisQuestion);
 //                            $('#questions').append(function(index,html){
 //                                    return MobileQuestionsUtils.createQuestion(thisQuestion);
 //                                });
-						}
-					},
-					error: function(data) {
-						alert('Error Status: ' + data["statusText"]);
 					}
-				});
-			},
-            
-            createQuestion : function(question) {
-                var questionHtml = '<p/>' + question["question"];
-				if (question["answerType"] === "ENUM")
-				{
-					answerId = "answer_"+i;
-					questionHtml += "<input id='"+answerId+"'></div>";
+				},
+				error: function(data) {
+					alert('Error Status: ' + data["statusText"]);
 				}
-                return questionHtml;
-            }
-            
-  		}
+			});
+		},
+		
+		createQuestion : function(question) {
+			var questionHtml = '<p/>' + question["question"];
+			if (question["answerType"] === "ENUM")
+			{
+				answerId = "answer_"+i;
+				questionHtml += "<input id='"+answerId+"'></div>";
+			}
+			return questionHtml;
+		}
+		
  	}
 
 	return MobileQuestions;
 
 })();
 
-var MobileQuestionsUtils = MobileQuestions.Util;
+var MobileQuestionsUtils = MobileQuestions;
 
 $(document).ready(function() {
     $('#templates').hide();
